2bda674d0b1daddc197d41cdb25577ea8bc66d58
Sudharshan Vijay SK <98068200+Spark1810@users.noreply.github.com>
2025-03-02 22:04:17 +0530

    Add files via upload

---
+++ SMARTCROP RECOMMENDATION SYSTEM.py
@@ -0,0 +1,2 @@
+import os
+os.system(r'cmd /c "streamlit run "C:\Users\Admin\Desktop\Working\SmartCrop Intelligent Crop Recommendation System\SmartCrop Intelligent Crop Recommendation System\SmartCrop Intelligent Crop Recommendation System\app.py"" ')
